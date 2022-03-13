1d0b7ab35cd7394e149f367176b8cfd1da00c738
TonnieAfric <adewaleoluwatonitn@gmail.com>
2022-03-13 20:47:23 +0100

    positve or negative

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return Always 0 (Success)
+ */
+int main(void)
+{
+	@@ -16,7 +16,7 @@ int main(void)
+
+	if (n == 0)
+		printf("%d is zero\n", n);
+	else if ( n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is positive\n", n);
+	return (0);
+}
