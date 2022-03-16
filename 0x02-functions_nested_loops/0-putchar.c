d8b92648ed9f5ad759afa793ec3afc5a5de59334
TonnieAfric <adewaleoluwatonitn@gmail.com>
2022-03-16 02:47:19 +0100

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints _putchar followed by a new line character
+ * Return: zero (0) success
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
